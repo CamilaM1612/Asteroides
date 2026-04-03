b801429dcd0eb3b050c1bc10e1b9a338be6002c5
Camila Muñoz <camilamo576@gmail.com>
2026-04-03 12:58:45 -0400

    se cambio el fondo del juego, se agrego un circulo simulando asteroide

--- script.js
+++ script.js
@@ -5,11 +5,18 @@ const nave = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const x = canvas.width / 2;
-const y = canvas.height /2;
+const x = canvas.width / 2 - 50/2;
+const y = canvas.height /2 - 50/2;
 
-
+//redondo
 nave.strokeStyle = "white";
-nave.lineWidth = 5;
-nave.strokeRect(x, y, 50, 50);
+nave.lineWidth = 2;
+nave.beginPath();
+nave.arc(95, 50, 40, 0, 2 * Math.PI);
+nave.stroke();
+
+//cuadrado
+nave.fillStyle = "white"
+nave.font = "50px Arial";
+nave.fillText("A",x,y);
 
