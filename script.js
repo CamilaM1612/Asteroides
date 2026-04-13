40211fdde5bc7360cb1ed6d08403265b5f6e7a1b
Camila Muñoz <camilamo576@gmail.com>
2026-04-13 19:16:24 -0400

    ya se puede mover la nave, solo con los teclados de flechas

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
 const canvas = document.getElementById("juego");
 const ctx = canvas.getContext("2d");
 
-// lienzo
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const x = canvas.width / 2;
-const y = canvas.height / 2;
-
 // crear imagen
 const naveImg = new Image();
-naveImg.src = "imagenes/Nave.png";
+naveImg.src = "imagenes/naveJuego.png";
 
-naveImg.onload = function () {
-  startGame();
+const miNave = {
+    width: 70,
+    height: 100,
+    x: window.innerWidth / 2 - 70 / 2,
+    y: window.innerHeight / 2 - 100 / 2,
+    speedX: 0,
+    speedY: 0
 };
 
-function startGame() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(naveImg, x, y, 60, 60);
+naveImg.onload = function () {
+  miAreaJuego.start();
+};
+
+
+var miAreaJuego = {
+    canvas: canvas,
+    
+    start: function (){
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        this.ctx = this.canvas.getContext("2d");
+        
+        this.intervalo = setInterval (actualizarArea, 20);
+        
+        window.addEventListener('keydown', function (e) {
+            miAreaJuego.key = e.keyCode;
+        });
+        window.addEventListener('keyup', function (){
+            miAreaJuego.key = false;
+        });
+    }, 
+    clear : function (){
+        this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
+    }
+}
+
+function actualizarArea (){
+    miAreaJuego.clear();
+
+    miNave.speedX = 0;
+    miNave.speedY = 0;
+
+    if(miAreaJuego.key == 37){ miNave.speedX = -2;}
+    if(miAreaJuego.key == 39){ miNave.speedX = 2;}
+    if(miAreaJuego.key == 38){ miNave.speedY = -2;}
+    if(miAreaJuego.key == 40){ miNave.speedY = 2;}
+    
+    miNave.x += miNave.speedX;
+    miNave.y += miNave.speedY;
+
+    ctx.drawImage(naveImg, miNave.x, miNave.y, miNave.width, miNave.height)
 }
